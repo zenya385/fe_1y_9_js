c5b29abd7852d607b9cc6a1524e31fd73be2649b
Zeno Roman <zeno385@gmail.com>
2023-05-31 17:44:04 +0300

    JS main

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
-const string = 'JavaScript is awesome';
-console.log(string.length);
-
-// let num = 2;
-// console.log('num:>>', num);
-
-// num = 5;
-// console.log('num:>>', num);
-
-let str;
-
-console.log('str:>>', str);
+'use strict';
