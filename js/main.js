96da495bc1031aad6078a3f0adf40637b129ebb1
Zeno Roman <zeno385@gmail.com>
2023-06-07 17:05:11 +0300

    type

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const string = 'JavaScript is awesome';
+// const string = 'JavaScript is awesome';
 // console.log(string.length);
 
 // let num = 2;
@@ -7,12 +7,81 @@ const string = 'JavaScript is awesome';
 // num = 5;
 // console.log('num:>>', num);
 
-let str;
-const publicUser = 2;
+// let str;
+// const publicUser = 2;
 
-// console.log('str:>>', str);
+// console.log('log:>>', publicUser);
+// console.warn('warn:>>', publicUser);
+// console.error('error:>>', publicUser);
 
-// console.warn('num:>>', num);
-// console.info('num:>>', num);
-console.log('publicUser:>>', publicUser);
-// console.error('num:>>', num);
+// let user = '';
+// console.log('user:>>', user);
+
+// let age = 0;
+// age = 12;
+// user = 'Ivan';
+// console.log('user:>>', user);
+// console.log('age:>>', age);
+
+// const asf = 'sdf';
+
+// const userName = 'Vasul';
+
+// const userNameProfileYoutube = 'qwerty';
+
+// const num = 20; //* number ===========
+// console.log('num:>>', num);
+// console.log('Type:>>', typeof num);
+
+// const string = 'Hi!'; //* string =========
+// console.log('string:>>', string);
+// console.log('Type:>>', typeof string);
+
+// const isOpenModal = false; //false або true  //* boolean ===========
+// console.log('isOpenModal:>>', isOpenModal);
+// console.log('Type:>>', typeof isOpenModal);
+
+// const numberNull = null; //* null
+// console.log('null:>>', numberNull);
+// console.log('Type:>>', typeof numberNull);
+
+// let ageUser; //* undefined
+// console.log('undefined:>>', ageUser);
+// console.log('Type:>>', typeof ageUser);
+
+//* ======================================= Метод console.log() ====================
+const name = 'Roman';
+const message = 'JavaScript is awesome!';
+
+// console.log('name =>', name);
+// window.console.log();
+
+//* ======================================= Метод alert.log() ====================
+// alert(message);
+// window.alert(message);
+
+//* ======================================= Метод confirm.log() ===================
+//3  confirm() - виводить модальне вікно з повідомленням, і дві кнопки, Ok і Cancel. Натискаючи на Ok,
+//3  результатом будет true, натискаючи на Cancel - повертається false.
+
+// const isComin = confirm('Please confirm hotel reservation');
+// console.log('isComin:>>', isComin);
+// console.log('isComin:>>', confirm('Please confirm hotel reservation'));
+
+//* ======================================= Метод prompt.log() ====================
+//3 prompt() - виводить модальне вікно з полем введення і кнопками Ok і Cancel. Натискаючи на Ok,
+//3 результатом буде те, що ввів користувач, у випадку Cancel - повертається null.
+
+// const value = prompt('Please enter your name');
+// console.log('value:>>', value);
+
+// console.log(typeof prompt('Please enter your name'));
+
+// prompt("Привіт! Введи своє ім'я.");
+const userName = prompt("Введи своє ім'я.");
+// alert('Привіт!' + ' ' + userName);
+// alert('Привіт! ' + userName);
+alert(`Привіт!         ${userName}`);
+// console.log(prompt("Введи своє ім'я."));
+
+// =====================================================================================================
