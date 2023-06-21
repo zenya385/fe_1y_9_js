3f405cda11cefe5ab16f98060d893a534c2a43e6
Zeno Roman <zeno385@gmail.com>
2023-06-21 20:03:17 +0300

    add DOM

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
 'use strict';
+const obj = {
+  name: 'Roman',
+  age: 15,
+  favorite: 'red',
+  isStudent: false,
+};
+
+const array = [
+  {
+    name: 'Roman',
+    age: 15,
+    favorite: 'red',
+    isStudent: false,
+  },
+  {
+    name: 'Sergio',
+    age: 20,
+    favorite: 'green',
+    isStudent: true,
+  },
+  {
+    name: 'Max',
+    age: 25,
+    favorite: 'yellow',
+    isStudent: false,
+  },
+];
+
+console.log('obj.name:>>', obj.name);
+console.log('obj.name:>>', array[2].favorite);
+
+document.body.style.backgroundColor = 'red'; //Змінює фон нашої сторінки
+
+const mainHead = document.querySelector('#head');
+// const mainHead = document.querySelector('li');
+mainHead.textContent = 'Я змінив заголовок!!!'; //Змінює заголовок h1
+mainHead.style.color = 'yellowGreen'; //Змінює колір заголовоку h1
+
+const item = document.querySelectorAll('li');
+item.forEach(li => {
+  li.classList.add('yellow-text'); // Добавляє клас yellow-text всім li
+  li.style.color = 'white'; //Змінює колір li
+});
+
+// console.log('item:>>', item);
