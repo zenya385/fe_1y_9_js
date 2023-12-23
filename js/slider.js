3590a85a4e660d1883599c025badda1989ee3171
Zeno Roman <zeno385@gmail.com>
2023-12-23 09:58:14 +0200

    add

---
+++ js/slider.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  $('.slider-team').slick({
+    //     // setting-name: setting-value
+    dots: true, // крапки
+    infinite: true,
+    speed: 300,
+    arrows: true, // стрілки
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    adaptiveHeight: true, // адаптивна висота слайдера
+    centerMode: true,
+    // variableWidth: true,
+    // centerPadding: '10px',
+    // rows:1, //
+    // slidesPerRow:
+    // easing: 'linear',
+    autoplay: true,
+    // autoplaySpeed: 2000,
+    // pauseOnDotsHover: true,
+    // pauseOnHover: true,
+    // pauseOnFocus: true,
+    // draggable: false,
+    // swipe:false,
+  });
+});
+
+// https://www.youtube.com/watch?v=rXNfDfqtM3M
