b74866518f093dae27a559365ed8431769d21c66
Roman Zeno <zeno385@gmail.com>
2024-01-24 19:36:55 +0200

    lazyload

--- js/03-lazy-load.js
+++ js/03-lazy-load.js
@@ -4,13 +4,13 @@
  * - події завантаження зображень
  */
 
-const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+// const lazyImages = document.querySelectorAll('img[loading="lazy"]');
 
-lazyImages.forEach(image => {
-  image.addEventListener('load', onImageLoaded, { once: true });
-});
+// lazyImages.forEach(image => {
+//   image.addEventListener('load', onImageLoaded, { once: true });
+// });
 
-function onImageLoaded(evt) {
-  console.log('Картинка загрузилась');
-  evt.target.classList.add('appear');
-}
+// function onImageLoaded(evt) {
+//   console.log('Картинка загрузилась');
+//   evt.target.classList.add('appear');
+// }
--- js/04-lazysizes.js
+++ js/04-lazysizes.js
@@ -1,5 +1,5 @@
 /*
- * Библиотека lazysizes
+ * Бібліотека lazysizes
  * - feature detection
  */
 
@@ -11,21 +11,20 @@ if ('loading' in HTMLImageElement.prototype) {
   addLazySizesScript();
 }
 
-const lazyImages = document.querySelectorAll('img[data-src]');
+// const lazyImages = document.querySelectorAll('img[data-src]');
 
-lazyImages.forEach(image => {
-  image.addEventListener('load', onImageLoaded, { once: true });
-});
-
-function onImageLoaded(evt) {
-  console.log('Картинка загрузилась');
-  evt.target.classList.add('appear');
-}
+// lazyImages.forEach(image => {
+//   image.addEventListener('load', onImageLoaded, { once: true });
+// });
+//
+// function onImageLoaded(evt) {
+//   console.log('Картинка загрузилась');
+//   evt.target.classList.add('appear');
+// }
 
 function addLazySizesScript() {
   const script = document.createElement('script');
-  script.src =
-    'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.2/lazysizes.min.js';
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.2/lazysizes.min.js';
   script.integrity =
     'sha512-TmDwFLhg3UA4ZG0Eb4MIyT1O1Mb+Oww5kFG0uHqXsdbyZz9DcvYQhKpGgNkamAI6h2lGGZq2X8ftOJvF/XjTUg==';
   script.crossOrigin = 'anonymous';
