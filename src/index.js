b47424cfd927126fbee51f34b20327a4e38a934b
Roman Zeno <zeno385@gmail.com>
2024-05-24 20:08:12 +0300

    add async/await

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 
+// getFruits('apple').then(data=>data.json()).then(apple=>console.log('apple =>', apple)).catch()
+
 
 // async/await
 
+// try {
+  
+// } catch (error) {
+  
+// }
 
 function getFruits(name) {
   const fruits = {
@@ -14,7 +21,7 @@ function getFruits(name) {
   // return Promise.resolve(fruits[name])
 
 return new Promise((resolve, reject)=>
-setTimeout(resolve(fruits[name]), 1500)
+setTimeout(()=>resolve(fruits[name]), 500)
 )
 }
 
@@ -22,28 +29,68 @@ setTimeout(resolve(fruits[name]), 1500)
 
 
 async function makeSmoothi() {
-  console.time('makeSmoothi')
 
-const apple = await getFruits('apple')
-console.log(apple);
-const kiwi =  await getFruits('kiwi')
-console.log(kiwi);
-const strawberry =  await getFruits('strawberry')
-console.log(strawberry);
-  
-//  getFruits('apple').then(apple=>{
-//   console.log('apple =>', apple);
-//   getFruits('kiwi').then(kiwi=>{
-//     console.log('kiwi =>', kiwi);
-//     getFruits('strawberry').then(strawberry=>
-//     {console.log('strawberry =>', strawberry)})
-//   })
-// })
-console.timeEnd('makeSmoothi')
-// return fruits
+  try {
+    console.time('makeSmoothi')
+
+    const apple =  getFruits('apple')
+    // console.log(apple);
+    const kiwi =   getFruits('kiwi')
+    // console.log(kiwi);
+    const berry =   getFruits('strawberry')
+    // console.log(berry);
+      
+    //  getFruits('apple').then(apple=>{
+    //   console.log('apple =>', apple);
+    //   getFruits('kiwi').then(kiwi=>{
+    //     console.log('kiwi =>', kiwi);
+    //     getFruits('strawberry').then(strawberry=>
+    //     {console.log('strawberry =>', strawberry)})
+    //   })
+    // })
+    const fruits = await Promise.all([apple, kiwi, berry])
+    console.log('fruits :>> ', fruits);
+    
+    
+    console.timeEnd('makeSmoothi')
+    return fruits
+  } catch (error) {
+    console.log('помилка :>> ', error);
+  }
+
 }
 
 
 makeSmoothi()
 
 
+
+
+// Function declaration
+async function fn(params) {
+  // await
+}
+
+// Functional expression
+const fn = async function (){
+  // await
+}
+
+// Arrow function
+const fn = async ()=>{
+  // await
+}
+
+// Object method
+const obj = {
+ async fn(){
+  // await
+  }
+}
+
+// Class method
+class Hero {
+ async fn(){
+  // await
+ }
+}
